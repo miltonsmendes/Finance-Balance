35171201095bf61879919cc406495475596b9dfa
miltonsmendes <miltonsergiom@gmail.com>
2021-02-02 23:29:29 -0300

    update JS

--- script.js
+++ script.js
@@ -42,9 +42,15 @@ let Transaction = {
         App.reload()
     },
 
+    remove(index){
+        Transaction.all.splice(index, 1)
+
+        App.reload()
+    },
+
     incomes() {
         let income = 0;
-        transactions.forEach(transaction =>{
+        Transaction.all.forEach(transaction =>{
             if (transaction.amount > 0 ) {
                 income += transaction.amount;
             } 
@@ -55,7 +61,7 @@ let Transaction = {
 
     expenses() {
         let expense = 0;
-        transactions.forEach(transaction =>{
+        Transaction.all.forEach(transaction =>{
             if (transaction.amount < 0 ) {
                 expense += transaction.amount;
             } 
@@ -66,7 +72,7 @@ let Transaction = {
 
     total(){
         return this.incomes() + this.expenses();
-    }
+    },
 
 }
 
@@ -77,11 +83,13 @@ let DOM = {
 
     addTransaction(transaction, index){
         const tr = document.createElement('tr')
-        tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction, index)
+        tr.dataset.index = index
+
         DOM.transactionsContainer.appendChild(tr)
     },
 
-    innerHTMLTransaction(transaction) {
+    innerHTMLTransaction(transaction, index) {
 
         const CSSclass = transaction.amount > 0 ? "income" : "expense"
 
@@ -91,7 +99,7 @@ let DOM = {
             <td class="description">${transaction.description}</td>
             <td class="${CSSclass}">${amount}</td>
             <td class="date">${transaction.date}</td>
-            <th><i class="fas fa-minus-circle"></th>
+            <th><i onclick="Transaction.remove(${index})" class="fas fa-minus-circle"></th>
         </tr>
         `
         return html
@@ -117,6 +125,16 @@ let DOM = {
 
 // OBJETO COM FUNÇÕES DE UTILIDADES (EX: FORMATAR NÚM P/ MOEDA)
 const Utils = {
+    formatAmount(value){
+        value = Number(value) * 100
+        return value
+    },
+
+    formatDate(date){
+       const splittedDate = date.split("-")
+       return `${splittedDate[2]}/${splittedDate[1]}/${splittedDate[0]}`
+    },
+
     formatCurrency(value){
         const signal = Number(value) < 0 ? "-" : ""
         
@@ -132,13 +150,74 @@ const Utils = {
     }
 }
 
+const Form = {
+    description: document.querySelector('input#description'),
+    amount: document.querySelector('input#amount'),
+    date: document.querySelector('input#date'),
+
+    getValues(){
+        return {
+            description: Form.description.value,
+            amount: Form.amount.value,
+            date: Form.date.value
+        }
+    },
+     
+    
+    validateFields(){
+        const { description, amount, date } = Form.getValues()
+        
+        if ( description.trim() === "" || 
+             amount.trim() === "" ||
+             date.trim() === "") {
+                throw new Error("Por favor, preencha todos os campos")
+
+        }
+    },
+
+    formatValues(){
+        let { description, amount, date } = Form.getValues()
+
+        amount = Utils.formatAmount(amount)
+        date = Utils.formatDate(date)
+
+        return {
+            description,
+            amount,
+            date
+        }
+    },
+
+    clearFields(){
+        Form.description.value = ""
+        Form.amount.value = ""
+        Form.date.value = ""
+    },
+
+
+    submit(event){
+        event.preventDefault()
+
+        try{
+            Form.validateFields()
+            const transaction = Form.formatValues()
+            Transaction.add(transaction)
+            Form.clearFields()
+            layer.close()
+
+        } catch (error){
+            alert(error.message)
+        }
+
+        
+    }
+}
+
 const App = {
     init(){
 
         // FAZ O PREENCHIMENTO DA TABELA
-        Transaction.all.forEach(transaction =>{
-            DOM.addTransaction(transaction)
-        })
+        Transaction.all.forEach(DOM.addTransaction)
 
         DOM.updateBalance()
 
@@ -150,7 +229,6 @@ const App = {
 }
 
 App.init()
-
 
 
 
